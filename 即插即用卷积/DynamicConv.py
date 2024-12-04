66394beeb7c100c5eb5412281b41bec18ab2af29
shayudiandian <kkfw03@163.com>
2024-12-04 09:19:14 +0800

    更新DynamicConv

---
+++ 即插即用卷积/DynamicConv.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from timm.models.layers import CondConv2d
+
+class DynamicConv(nn.Module):
+    """
+    Dynamic Conv layer
+    将额外的参数带入到网络
+    """
+
+    def __init__(self, in_features, out_features, kernel_size=1, stride=1, padding='', dilation=1,
+                 groups=1, bias=False, num_experts=4):
+        super().__init__()
+        print('+++', num_experts)
+        self.routing = nn.Linear(in_features, num_experts)
+        self.cond_conv = CondConv2d(in_features, out_features, kernel_size, stride, padding, dilation,
+                                    groups, bias, num_experts)
+
+    def forward(self, x):
+        pooled_inputs = F.adaptive_avg_pool2d(x, 1).flatten(1)  # CondConv routing
+        routing_weights = torch.sigmoid(self.routing(pooled_inputs))
+        x = self.cond_conv(x, routing_weights)
+        return x
