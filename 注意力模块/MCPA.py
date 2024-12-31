06e39fd713130cb00515e0290b8274b99f7e1a83
shayudiandian <kkfw03@163.com>
2024-12-31 10:07:02 +0800

    更新MCPA

---
+++ 注意力模块/MCPA.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+
+class MCrossAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0.1, proj_drop=0.1):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.wq = nn.Linear(head_dim, dim , bias=qkv_bias)
+        self.wk = nn.Linear(head_dim, dim , bias=qkv_bias)
+        self.wv = nn.Linear(head_dim, dim , bias=qkv_bias)
+#         self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim * num_heads, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+
+        B, N, C = x.shape
+        q = self.wq(x[:, 0:1, ...].reshape(B, 1, self.num_heads, C // self.num_heads)).permute(0, 2, 1, 3)  # B1C -> B1H(C/H) -> BH1(C/H)
+        k = self.wk(x.reshape(B, N, self.num_heads, C // self.num_heads)).permute(0, 2, 1, 3)  # BNC -> BNH(C/H) -> BHN(C/H)
+        v = self.wv(x.reshape(B, N, self.num_heads, C // self.num_heads)).permute(0, 2, 1, 3)  # BNC -> BNH(C/H) -> BHN(C/H)
+        attn = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+#         attn = (q @ k.transpose(-2, -1)) * self.scale  # BH1(C/H) @ BH(C/H)N -> BH1N
+        attn = attn.softmax(dim=-1)
+#         attn = self.attn_drop(attn)
+        x = torch.einsum('bhij,bhjd->bhid', attn, v).transpose(1, 2)
+#         x = (attn @ v).transpose(1, 2)
+        x = x.reshape(B, 1, C * self.num_heads)   # (BH1N @ BHN(C/H)) -> BH1(C/H) -> B1H(C/H) -> B1C
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
