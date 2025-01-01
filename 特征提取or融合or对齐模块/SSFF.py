30acb478ba80caa4b4c3ae2439bb04daeeecd3ac
shayudiandian <kkfw03@163.com>
2025-01-01 21:05:33 +0800

    更新SSFF

---
+++ 特征提取or融合or对齐模块/SSFF.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    # Pad to 'same' shape outputs
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+class Conv(nn.Module):
+    # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
+
+class Zoom_cat(nn.Module):
+    def __init__(self, in_dim):
+        super().__init__()
+        #self.conv_l_post_down = Conv(in_dim, 2*in_dim, 3, 1, 1)
+
+    def forward(self, x):
+        """l,m,s表示大中小三个尺度，最终会被整合到m这个尺度上"""
+        l, m, s = x[0], x[1], x[2]
+        tgt_size = m.shape[2:]
+        l = F.adaptive_max_pool2d(l, tgt_size) + F.adaptive_avg_pool2d(l, tgt_size)
+        #l = self.conv_l_post_down(l)
+        # m = self.conv_m(m)
+        # s = self.conv_s_pre_up(s)
+        s = F.interpolate(s, m.shape[2:], mode='nearest')
+        # s = self.conv_s_post_up(s)
+        lms = torch.cat([l, m, s], dim=1)
+        return lms
+
+
+class SSFF(nn.Module):
+    def __init__(self, channel):
+        super(SSFF, self).__init__()
+        self.conv1 =  Conv(512, channel,1)
+        self.conv2 =  Conv(1024, channel,1)
+        self.conv3d = nn.Conv3d(channel,channel,kernel_size=(1,1,1))
+        self.bn = nn.BatchNorm3d(channel)
+        self.act = nn.LeakyReLU(0.1)
+        self.pool_3d = nn.MaxPool3d(kernel_size=(3,1,1))
+
+    def forward(self, x):
+        p3, p4, p5 = x[0],x[1],x[2]
+        p4_2 = self.conv1(p4)
+        p4_2 = F.interpolate(p4_2, p3.size()[2:], mode='nearest')
+        p5_2 = self.conv2(p5)
+        p5_2 = F.interpolate(p5_2, p3.size()[2:], mode='nearest')
+        p3_3d = torch.unsqueeze(p3, -3)
+        p4_3d = torch.unsqueeze(p4_2, -3)
+        p5_3d = torch.unsqueeze(p5_2, -3)
+        combine = torch.cat([p3_3d,p4_3d,p5_3d],dim = 2)
+        conv_3d = self.conv3d(combine)
+        bn = self.bn(conv_3d)
+        act = self.act(bn)
+        x = self.pool_3d(act)
+        x = torch.squeeze(x, 2)
+        return x
